dc036a8ca7ab0dca05ae4f690367d8f518110bcf
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-03 22:44:21 +0300

    added Task003 Sum random

--- Task003_Sum/Program.cs
+++ Task003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int number1 = 3;
-int number2 = 7;
+﻿int number1 = new Random().Next(1, 10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 Console.Write(number1 + number2);
