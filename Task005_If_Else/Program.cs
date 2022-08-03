8c9047aaffc79c04e313702a976178225b6594c6
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-03 22:59:59 +0300

    added Task005 If Else

---
+++ Task005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "maksim")
+{
+    Console.WriteLine("Привет, дорогой!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
