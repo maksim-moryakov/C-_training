dfc2f1d7d5422b25ba9a71b3393a7b88c6c08d9f
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-03 23:11:35 +0300

    added Task006 Task about kettlebell

---
+++ Task006_kettlebell/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 4;
+int c = 3;
+int d = 7;
+int e = 6;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальный вес гири: ");
+Console.Write(max);
+
