8d6a8646f94044097b9c0858f1776609b3eb120a
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-08-06 11:36:56 +0300

    added Task008_seminar

---
+++ Task008_seminar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Text;
+
+
+Console.WriteLine("Input week day number");
+
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Enum.GetName(typeof(DayOfWeek),weekday));
+
+
+var rusculture = new System.Globalization.CultureInfo("ru-RUS");
+DateTimeFormatInfo rusDateFormat = rusculture.DateTimeFormat;
+
+string[] rusDays = rusDateFormat.DayNames;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine(rusDays[weekday]);
